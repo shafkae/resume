6a5ee988ccccb726b62cb6d91ba551bfecf2d3c9
shafkae <64600984+shafkae@users.noreply.github.com>
2022-08-29 18:16:36 -0400

    charts and about me (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { TitleCardComponent } from './components/title-card/title-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { ContentComponent } from './components/content/content.component';
+import { EducationComponent } from './components/education/education.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleCardComponent
+    TitleCardComponent,
+    ContentComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +23,14 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatToolbarModule,
     MatCardModule,
     MatGridListModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
